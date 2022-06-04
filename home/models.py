3f627d8e48dd85f2a78232c34d0d80daa3134ed1
Allen <edgarluis21@gmail.com>
2022-06-03 21:53:18 -0400

    test wailtail

---
+++ home/models.py
@@ -0,0 +1,102 @@
+from django.db import models
+
+from wagtail.core.models import Page
+
+from wagtail.core.fields import RichTextField
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.models import Page, Orderable
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.models import Page
+from wagtail.search import index
+
+class HomePage(Page):
+    """home page main"""
+
+    templates="home/home_page.html"
+    max_count =1
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+
+    banner_subtitle = RichTextField(features=["bold","italic"],default="Some String")
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+")
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+")
+    
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        ImageChooserPanel("banner_image"),
+        PageChooserPanel("banner_cta"),
+        InlinePanel('gallery_images', label="Gallery images"),
+        
+    ]
+    
+    #body = RichTextField(blank=True)
+    #content_panels = Page.content_panels + [FieldPanel('body', classname="full"),]
+class OrientePage(Page):
+    nombre_playa = RichTextField(features=["bold","italic"],default="nombre de la playa")
+    ubicacion_playa = RichTextField(features=["bold","italic"],default="direccion de la playa")
+    body = RichTextField(blank=True)
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel("nombre_playa"),
+        FieldPanel("ubicacion_playa"),
+        FieldPanel('body', classname="full"),
+
+        
+    ]
+
+class CentroPage(Page):
+    nombre_playa = RichTextField(features=["bold","italic"],default="nombre de la playa")
+    ubicacion_playa = RichTextField(features=["bold","italic"],default="direccion de la playa")
+    body = RichTextField(blank=True)
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel("nombre_playa"),
+        FieldPanel("ubicacion_playa"),
+        FieldPanel('body', classname="full"),
+
+            
+        ]
+
+class OccidentePage(Page):
+    nombre_playa = RichTextField(features=["bold","italic"],default="nombre de la playa")
+    ubicacion_playa = RichTextField(features=["bold","italic"],default="direccion de la playa")
+    body = RichTextField(blank=True)
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel("nombre_playa"),
+        FieldPanel("ubicacion_playa"),
+        FieldPanel('body', classname="full"),
+
+            
+        ]
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
+
